9892536bbad76dcc3c221ccc491acbc294bcbc7f
Adarsh <adarshkumar167@gmail.com>
2022-02-13 14:35:57 +0530

    110

--- 101-110/110.cpp
+++ 101-110/110.cpp
@@ -13,3 +13,58 @@ struct Node{
         right = NULL;
     }
 };
+
+bool isBST(Node* root, Node* min=NULL, Node* max=NULL){
+    if(root == NULL){
+        return true;
+    }
+    if(min != NULL && root->data <= min->data){
+        return false;
+    }
+    if(max!= NULL && root->data >= max->data){
+        return false;
+    }
+    
+    bool leftValid = isBST(root->left, min, root);
+    bool rightValid = isBST(root->right, root, max);
+    return (leftValid && rightValid);
+}
+
+int main(){
+    
+   Node *root1 = new Node(1);
+   root1->left = new Node(2);
+   root1->right = new Node(3);
+
+   /*
+                1
+               / \
+              2   3
+    */
+
+   if(isBST(root1, NULL, NULL)){
+       cout << "Valid BST" << endl;
+   }
+   else{
+       cout << "InValid BST" << endl;
+   }
+
+   /*
+                5
+               / \
+              2   8
+    */
+
+   Node *root2 = new Node(5);
+   root2->left = new Node(2);
+   root2->right = new Node(8);
+
+   if(isBST(root2, NULL, NULL)){
+       cout << "Valid BST" << endl;
+   }
+   else{
+       cout << "InValid BST" << endl;
+   }
+
+   return 0;
+}
