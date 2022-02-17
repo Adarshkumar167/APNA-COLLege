ef394e422056fedf26fe4754cdc1baf53e30a927
Adarsh <adarshkumar167@gmail.com>
2022-02-18 02:59:28 +0530

    changed location

--- 111-120/112.cpp
+++ 111-120/112.cpp
@@ -7,10 +7,12 @@ int catalan(int n){
     if(n <= 1){
         return 1;
     }
+    
     int res = 0;
     for(int i = 0; i <= n - 1; i++){
         res += catalan(i) * catalan(n-i-1);
     }
+
     return res;
 }
 
@@ -18,5 +20,6 @@ int main(){
     for(int i = 0; i < 10; i++){
         cout << catalan(i) << " ";
     } cout << endl;
+
     return 0;
 }
