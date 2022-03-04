b767e928013717e03adbef496028f3785873bb35
Adarsh <adarshkumar167@gmail.com>
2022-03-04 17:23:58 +0530

    120

--- 111-120/120.cpp
+++ 111-120/120.cpp
@@ -1,62 +1,48 @@
 // Painters Partition Problem
 
-// Page Allocation Problem
-
 #include <iostream>
-#include <climits>
 #include <algorithm>
 using namespace std;
 
-bool isPossible(int arr[], int n, int m, int min){
-    int studentsRequired = 1, sum = 0;
+int findFeasible(int boards[], int n, int limit){
+    int painters = 1, sum = 0;
     for(int i = 0; i < n; i++){
-        if(arr[i] > min){
-            return false;
-        }
-        if(sum+arr[i] > min){
-            studentsRequired++;
-            sum = arr[i];
-
-            if(studentsRequired > m){
-                return false;
-            }
-        }
-        else{
-            sum += arr[i];
+        sum += boards[i];
+        if(sum > limit){
+            sum = boards[i];
+            painters++;
         }
     }
-    return true;
+    return painters;
 }
 
-int allocateMinimumPages(int arr[], int n, int m){
-    int sum = 0;
-    if(n < m){
-        return -1;
-    }
+int paintersPartition(int boards[], int n, int m){
+    int totalLength = 0, k = 0;
     for(int i = 0; i < n; i++){
-        sum = sum + arr[i];
+        k = max(k, boards[i]);
+        totalLength += boards[i];
     }
-    int start = 0, end = sum, ans = INT_MAX;
-    while(start <= end){
-        int mid = (start + end)/2;
-        if(isPossible(arr, n, m, mid)){
-            ans = min(ans, mid);
-            end = mid-1;
+
+    int low = k, high = totalLength;
+    while(low < high){
+        int mid = (low + high)/2;
+        int painters = findFeasible(boards, n, mid);
+        if(painters <= m){
+            high = mid;
         }
         else{
-            start = mid+1;
+            low = mid + 1;
         }
     }
-    return ans;
+    return low;
 }
 
 int main(){
-    int arr[] = {12, 34, 67, 90};
+    int arr[] = {10, 20, 30, 40};
     int n = 4;
     int m = 2;
 
-    cout << "The minimum no. of pages : " << allocateMinimumPages(arr, n, m) << endl;
-
-
+    cout << "Minimum time to paint boards : " << paintersPartition(arr, n, m) << endl;
+    
     return 0;
 }
